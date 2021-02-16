080dc8b01e04255f6053708c96806e24c9b51640
TheTechTitan <telan17@gmail.com>
2021-02-16 18:11:19 +0530

    Having all models in commons to share across other services as dependency

---
+++ Microservices/07-production-ready-service/shoppingcloudcommons/src/main/java/com/infinity/shoppingcloud/commons/model/Customer.java
@@ -0,0 +1,60 @@
+package com.infinity.shoppingcloud.commons.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class  Customer {
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    String firstName;
+    String lastName;
+    String address;
+    String zipcode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+}
+
+
